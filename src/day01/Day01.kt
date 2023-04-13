7c157d080c9b2c59538efba54ca5dee924b77e03
Highlander Vilela de Paiva Silva <highlander.silva@bancointer.com.br>
2023-04-13 00:50:22 -0300

    day 01

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -4,13 +4,43 @@ import DayPuzzle
 
 fun main() {
     DayPuzzle<Int>("01")
-        .withParts(Part1)
+        .withParts(Part1, Part2)
         .solve()
 }
 
-
-object Part1 : DayPuzzle.PartPuzzle<Int>("Part 1", 5) {
+object Part1 : DayPuzzle.PartPuzzle<Int>("Part 1", 24000) {
     override fun solve(input: List<String>): Int {
-        return input.size
+        return groupBetweenEmpty(input).topNElves(1)
     }
+}
+
+object Part2 : DayPuzzle.PartPuzzle<Int>("Part 2", 45000) {
+    override fun solve(input: List<String>): Int {
+        return groupBetweenEmpty(input).topNElves(3)
+    }
+}
+
+fun groupBetweenEmpty(input: List<String>): List<List<Int>> {
+    val outputList = mutableListOf<MutableList<Int>>()
+    var tempList = mutableListOf<Int>()
+
+    for (value in input) {
+        if (value.isNotEmpty()) {
+            tempList.add(value.toInt())
+        } else {
+            outputList.add(tempList)
+            tempList = mutableListOf()
+        }
+    }
+    outputList.add(tempList)
+
+    return outputList
+}
+
+fun List<List<Int>>.topNElves(n: Int): Int {
+    return this
+        .map { it.sum() }
+        .sortedDescending()
+        .take(n)
+        .sum()
 }
