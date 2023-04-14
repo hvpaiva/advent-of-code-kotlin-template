1459e9f053cc958042e0445d07d5aa0a8cd099b7
Highlander Vilela de Paiva Silva <highlander.silva@bancointer.com.br>
2023-04-14 10:48:16 -0300

    day 01 template

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -4,43 +4,12 @@ import DayPuzzle
 
 fun main() {
     DayPuzzle<Int>("01")
-        .withParts(Part1, Part2)
+        .withParts(Part1)
         .solve()
 }
 
-object Part1 : DayPuzzle.PartPuzzle<Int>("Part 1", 24000) {
+object Part1 : DayPuzzle.PartPuzzle<Int>("Part 1", 10) {
     override fun solve(input: List<String>): Int {
-        return groupBetweenEmpty(input).topNElves(1)
+        return input.size
     }
-}
-
-object Part2 : DayPuzzle.PartPuzzle<Int>("Part 2", 45000) {
-    override fun solve(input: List<String>): Int {
-        return groupBetweenEmpty(input).topNElves(3)
-    }
-}
-
-fun groupBetweenEmpty(input: List<String>): List<List<Int>> {
-    val outputList = mutableListOf<MutableList<Int>>()
-    var tempList = mutableListOf<Int>()
-
-    for (value in input) {
-        if (value.isNotEmpty()) {
-            tempList.add(value.toInt())
-        } else {
-            outputList.add(tempList)
-            tempList = mutableListOf()
-        }
-    }
-    outputList.add(tempList)
-
-    return outputList
-}
-
-fun List<List<Int>>.topNElves(n: Int): Int {
-    return this
-        .map { it.sum() }
-        .sortedDescending()
-        .take(n)
-        .sum()
 }
